1cf81e980ca671a3a24ef1847387b21813abdb24
Joel McCune <knu2xs@gmail.com>
2020-10-23 17:12:36 -0700

    Bump version: 0.1.0-dev0 → 0.2.0-dev0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     name='placekey_arcgis',
     package_dir={"": "src"},
     packages=find_packages('src'),
-    version='0.1.0-dev0',
+    version='0.2.0-dev0',
     description='Enable Placekey API lookup against spatially enabled dataframes.',
     long_description=long_description,
     author='Joel McCune',
--- src/placekey_arcgis/__init__.py
+++ src/placekey_arcgis/__init__.py
@@ -1,5 +1,5 @@
 __title__ = 'placekey_arcgis'
-__version__ = '0.1.0-dev0'
+__version__ = '0.2.0-dev0'
 __author__ = 'Joel McCune'
 __license__ = 'Apache 2.0'
 __copyright__ = 'Copyright 2020 by Joel McCune'
